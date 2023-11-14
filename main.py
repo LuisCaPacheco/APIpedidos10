ff26350fe230e4984772620717be14bde37f58e9
LuisCaPacheco <lupacheco@utb.edu.co>
2023-11-14 17:42:33 -0500

    prueba #3

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, HTTPException
 from pydantic import BaseModel
 from starlette.templating import Jinja2Templates
+from typing import Optional
+import uuid
 
 app = FastAPI()
+
+usuarios = {}
+
+pedidos = {}
 
 class Order(BaseModel):
     product: str
     quantity: int
     customer: str
+
+class Usuario(BaseModel):
+    id: uuid.UUID
+    nombre: str
+    email: Optional[str] = None
+
+class Pedido(BaseModel):
+    id: uuid.UUID
+    usuario_id: uuid.UUID
+    descripcion: str
+    entregado: bool = False
+
 
 orders = []
 
@@ -25,3 +43,27 @@ def read_orders():
 @app.get("/order_form")
 def order_form(request: Request):
     return templates.TemplateResponse("order_form.html", {"request": request})
+
+def get_usuario(id: uuid.UUID):
+    usuario = usuarios.get(id)
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    return usuario
+
+@app.post("/usuarios/")
+def crear_usuario(usuario: Usuario):
+    usuarios[usuario.id] = usuario
+    return usuario
+
+@app.post("/pedidos/")
+def crear_pedido(pedido: Pedido, usuario: Usuario = Depends(get_usuario)):
+    pedido.usuario_id = usuario.id
+    pedidos[pedido.id] = pedido
+    return pedido
+
+@app.get("/pedidos/{pedido_id}")
+def obtener_pedido(pedido_id: uuid.UUID):
+    pedido = pedidos.get(pedido_id)
+    if not pedido:
+        raise HTTPException(status_code=404, detail="Pedido no encontrado")
+    return pedido
